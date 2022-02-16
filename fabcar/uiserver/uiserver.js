3cecc7cb64f1e589e47abe1a7a2dcbb2b338f84a
Filipe4010 <filipe.masini@outlook.com>
2022-02-16 14:56:29 -0300

    teste

--- fabcar/uiserver/uiserver.js
+++ fabcar/uiserver/uiserver.js
@@ -11,8 +11,8 @@ const path = require('path');
 const fs = require('fs');
 //const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
   //      const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-app.set("view engine","pug");
 
+app.set('view engine', 'handlebars');
 app.get('/api/', function (req, res) {
 
     res.render('index');
